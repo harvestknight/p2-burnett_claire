16fa3ec14131e21d8a4265370b3f0283c5b1d998
Claire Burnett <claireburnett@Claire--Trevor-iMac.local>
2018-11-01 17:27:28 -0600

    Convert code to ES6, write initial functionality to get users to choose a color

--- src/main.js
+++ src/main.js
@@ -1,17 +1,35 @@
 //Choose a color + loop through the number of times the name of the color is long
-const fortuneColor = document.getElementsByTagName('h1'); //or use document.getElementsByClassName('colors') and add a CSS 'color' class to the h1's
+const fortuneColor = document.getElementsByTagName('h1'); //or use document.getElementsByClassName('colors') and add a CSS 'color' class to the h1's --- use this instead...change h1's to p
 
-function runColor() {
-    this.length('x');
+//function runColor() {
+   // this.length('x');
+//};
+
+function functionWrapper () {
+   const runColor = () => {
+    
+    let thisColorText = this.childNodes[0].nodeValue;
+       
+    let thisColorLength = thisColorText.length;
+    
+    for (let i=0; i < thisColorLength; i++) {
+        console.log('hi');
+        //call class list on the variable and toggle that class....put sections inside of a variable to add them to...just start with 'add' class instead of toggle
+        //grab each section every time I click them and put them into an array variable...use loop to add classes
+        //this is where I put functionality based on button length
+        //set timeout method will come into play here to stagger CSS transition functionality
+    }
+   };  
+    runColor();
 };
 
-for (i=0; i < fortuneColor.length; i++) {
- fortuneColor[i].addEventListener('click', runColor);
+for (let i=0; i < fortuneColor.length; i++) {
+ fortuneColor[i].addEventListener('click', functionWrapper);
 };
 
 
 //Display numbers instead of colors
-
+    //will need a new event listener -- Blakeley will cover this live in Mod12 
 
 //Choose a number + loop through the same number of times as the number chosen
 
